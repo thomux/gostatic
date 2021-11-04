c29126a31a21119748a27b6587ec8e712234f3e3
Thomux <me@thomux.eu>
2021-11-04 10:05:16 +0100

    add test for the gostatic file

---
+++ gostatic/gostatic_test.go
@@ -0,0 +1,32 @@
+package gostatic
+
+import (
+	"log"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	root := "../test_data/"
+	config := DefaultConfig()
+	gs := New(root, config)
+
+	if gs.root != root {
+		log.Println("TEST: root path wrong")
+		t.Fail()
+	}
+
+	if gs.config != config {
+		log.Println("TEST: config wrong")
+		t.Fail()
+	}
+
+	if gs.templates == nil {
+		log.Println("TEST: templates is nil")
+		t.Fail()
+	}
+
+	if gs.structure == nil {
+		log.Println("TEST: structure is nil")
+		t.Fail()
+	}
+}
