68e653ccb5e700e8af82561001c6dc99975f60d0
Thomux <me@thomux.eu>
2021-11-06 11:29:10 +0100

    markdown tests

--- gostatic/markdown_test.go
+++ gostatic/markdown_test.go
@@ -1,11 +1,39 @@
 package gostatic
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
 func TestParseMarkdown(t *testing.T) {
-	t.Fail()
+	md := parseMarkdown("../test_data/_articles/article.md")
+	if len(md.Content) == 0 {
+		log.Println("TEST: content wrong")
+		t.Fail()
+	}
+
+	if len(md.Html) == 0 {
+		log.Println("TEST: html wrong")
+		t.Fail()
+	}
+
+	if md.Meta["title"].(string) != "A first article" {
+		log.Println("TEST: title wrong")
+		t.Fail()
+	}
+
+	if md.Meta["category"].(string) != "Server" {
+		log.Println("TEST: category wrong")
+		t.Fail()
+	}
 }
 
 func TestMdToHtml(t *testing.T) {
-	t.Fail()
+	data := "Hello\n\nWorld"
+	expect := "<p>Hello</p>\n<p>World</p>\n"
+	rendered := mdToHtml(data)
+	if rendered != expect {
+		log.Println("TEST: not equal", rendered)
+		t.Fail()
+	}
 }
